2a46a78ed49a236bfff6c0adf94664c9082e547e
Andrea Di Francia <84396483+andreadifra@users.noreply.github.com>
2026-05-08 18:05:56 +0100

    Add solution for pandas-read-csv

---
+++ study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def create_dataframe(data):
+    """
+    Returns: dict with 'data', 'shape', 'columns'
+    """
+    df = pd.DataFrame(data)
+    print(df)
+    return {"data" : df.to_dict("list"), "shape": list(df.shape), "columns": list(df.columns)}
